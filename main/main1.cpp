086f4c582b771e1db5f518960f91d147ea097483
pkisiel <pkisiel@gmail.com>
2020-10-10 11:27:36 +0200

    Examples

---
+++ main/main1.cpp
@@ -0,0 +1,9 @@
+// Source: https://en.cppreference.com/w/cpp/language/main_function
+// int main () { body }
+// The body of the main function does not need to contain the return statement: 
+// if control reaches the end of main without encountering a return statement, the effect is that of executing return 0;.
+
+int main()
+{
+    // with and without return 0
+}
---
+++ main/main2.cpp
@@ -0,0 +1,7 @@
+// int main (int argc, char *argv[]) { body }
+
+int main (int argc, char *argv[]) 
+{
+    
+    return 0; 
+}
