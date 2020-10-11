6b726588a6a39cc786382b4baad48cb23df61315
pkisiel <pkisiel@gmail.com>
2020-10-11 10:39:50 +0200

    C&R

--- main/main1.cpp
+++ main/main1.cpp
@@ -2,8 +2,12 @@
 // int main () { body }
 // The body of the main function does not need to contain the return statement: 
 // if control reaches the end of main without encountering a return statement, the effect is that of executing return 0;.
+// main return tasks
+// https://www.quora.com/What-is-return-0-in-C
 
 int main()
 {
+    // 1) Jak mozna sprawdzić
     // with and without return 0
+    // test only main() {}
 }
